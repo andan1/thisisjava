c58994b960b67d4bd1298aff405b958e1b49c8b7
andan1 <139194353+andan1@users.noreply.github.com>
2023-09-20 17:46:42 +0900

    Add files via upload

---
+++ Ch01/sec11/코드주석달기/Hello.java
@@ -0,0 +1,13 @@
+package Ch01.sec11.코드주석달기;
+
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}
+
+}
+// 행주석 : //
+// 범위 주석 : /*...*/
+// 도큐먼트 주석 : /**...*/ javadoc 명령어로 API 도큐먼트를 생성하는데 사용
---
+++ Ch01/sec12/실행문과세미콜론/Calculator.java
@@ -0,0 +1,13 @@
+package Ch01.sec12.실행문과세미콜론;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int x = 1;					// 변수 x를 선언하고 1을 저장
+		int y = 2;					// 변수 y를 선언하고 2를 저장
+		int result = x + y;			// 변수 result를 선언하고 x와 y를 더한 값을 저장
+		System.out.println(result);	// 콘솔에 출력하는 메소드 호출
+	}
+
+}
+// 실행문 끝에는 반드시 세미콜론을 붙여야 한다
