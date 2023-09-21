ee1097049d89fd16432b84a0d02c28452869e5f8
andan1 <139194353+andan1@users.noreply.github.com>
2023-09-21 15:28:10 +0900

    Add files via upload

---
+++ Ch04/sec04/for문/PrintFrom1To10Example.java
@@ -0,0 +1,12 @@
+package Ch04.sec04.for문;
+
+public class PrintFrom1To10Example {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + " ");
+		}
+	}
+
+}
+// for (초기화식, 조건식, 증감식) {...}
---
+++ Ch04/sec08/continue문/ContinueExample.java
@@ -0,0 +1,14 @@
+package Ch04.sec08.continue문;
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			if (i % 2 != 0) {
+				continue;
+			}
+			System.out.print(i + " ");
+		}
+	}
+
+}
